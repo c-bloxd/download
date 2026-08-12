ac777694103fcfd1ce13651350c44535aa12b8bf
GEORGECR0 <151453431+GEORGECR0@users.noreply.github.com>
2026-08-12 22:42:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+function downloadPack() {
+  const link = document.createElement("a");
+  link.href = "assets/tpacks/C_Bloxd 8x.zip";
+  link.download = "C_Bloxd 8x.zip";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function downloadAssetPack() {
+  const link = document.createElement("a");
+  link.href = "assets/tpacks/C_Bloxds Essentials.zip";
+  link.download = "C_Bloxd Essentials.zip";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function downloadRig() {
+  const link = document.createElement("a");
+  link.href = "assets/tpacks/Bob Rig Wide.bbmodel";
+  link.download = "Bob Rig Wide.bbmodel";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+document.querySelector("#Tpack-btn").addEventListener("click", function () {
+    downloadPack();
+});
+
+document.querySelector("#Asset-btn").addEventListener("click", function () {
+    downloadAssetPack();
+});
+
+document.querySelector("#Rig-btn").addEventListener("click", function () {
+    downloadRig();
+});
+
+document.querySelector(".tooltip").addEventListener("click", function () {
+    window.open("https://www.youtube.com/@_GEORGECR_", "_blank");
+});
+
+document.querySelector(".youtube-btn").addEventListener("click", function () {
+    window.open("https://www.youtube.com/@C_Bloxd", "_blank");
+});
+
+document.querySelector(".discord-btn").addEventListener("click", function () {
+    window.open("https://discord.com/invite/xaZjMWeFcr", "_blank");
+});
+
+document.querySelector(".jobs-btn").addEventListener("click", function () {
+    window.open("https://ytjobs.co/talent/profile/556663", "_blank");
+});
